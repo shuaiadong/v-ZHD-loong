6e0546f031b8e4cf063d33b37daa143609d6894f
v_zhonghuidong <v_zhonghuidong@baidu.com>
2018-11-21 16:07:25 +0800

    add docs 2018年11月21日 16:07:25

--- commit.sh
+++ commit.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env sh
 
 set -e
+
 git add .
+
 DATE=`date +"%Y年%m月%d日 %H:%M:%S"`
-# COMMIT = 
-# echo 
-# DATE = `date +%F
+echo "add docs ${DATE}"
 git commit -m "add docs ${DATE}"
-# git push
+
+git push
+echo "成功"
 cd -
