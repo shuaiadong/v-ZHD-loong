e9a424cf53d07654d0d184fa405b7a9450d9f4ae
v_zhonghuidong <v_zhonghuidong@baidu.com>
2018-11-21 13:20:52 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,45 @@
+
+module.exports = {
+    // header 文字
+    title: 'Do .',
+    // 网站描述
+    description: '不积跬步无以至千里，不积小流无以成江海。',
+    // ? 暂时未出来
+    // head: ['link', { rel: 'icon', href: `../icon.png` }]
+    // 端口 默认8080
+    port: 3000,
+    head: [
+        // 头部 icon
+        ['link', { rel: 'icon', href: `/logo.png` }],
+    ],
+    // 输出目录 默认 .vuepress/dist
+    dest: './docs/.vuepress/dists',
+    // // ga是谷歌推出的网站流量分析工具
+    // ga: ''
+    // 离线缓存
+    serviceWorker: true,
+
+    // 右侧导航
+    themeConfig: {
+        nav: [
+            {
+                text: 'ZHD',
+                link: '/'
+            },
+                { text: 'Github', link: '/guide/' },
+            //     { text: 'External', link: 'https://google.com' },
+        ],
+        sidebar: [
+            {
+                title: 'Dauly',
+                collapsable: false,
+                children: [
+                    ['daily/', 'Introduction'],
+                    'daily/day'
+                ]
+            },
+        ]
+    
+    },
+};
+
---
+++ docs/README.md
@@ -0,0 +1,14 @@
+---
+home: true
+heroImage: /timg.jpeg
+actionText: 开始浏览 →
+actionLink: /daily/
+features:   
+- title: 每日一看
+  details: 
+- title: 每日一记
+  details: 
+- title: 每日一敲
+  details: 
+footer: 。do .个人博客 。 
+---
